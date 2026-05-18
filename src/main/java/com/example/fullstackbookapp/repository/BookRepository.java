9a66b2f1ec0835231da307ec225db245e05b4201
otteun <pingpab_bing@naver.com>
2026-05-18 15:28:18 +0900

    clean: 깃허브 리셋

--- src/main/java/com/example/fullstackbookapp/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fullstackbookapp.repository;
-
-
-import com.example.fullstackbookapp.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
