0866b5b7715660edb193a3775321213d05a08a7d
otteun <pingpab_bing@naver.com>
2026-05-18 15:43:30 +0900

    fix: 최종 프로젝트 폴더 구조 업로드

---
+++ src/main/java/com/example/fullstackbookapp/FullstackBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackBookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackbookapp/FullstackBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
